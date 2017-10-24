ddc263e16b3de8aa2091a9d72159b2c99ae38607
Titus Moldovan <titus.moldovan@fortech.ro>
2017-10-24 09:25:37 +0300

    adds iOs view

--- StartProject.iOS/Views/LoginViewController.cs
+++ StartProject.iOS/Views/LoginViewController.cs
@@ -1,8 +1,8 @@
-using Foundation;
 using System;
 using UIKit;
 using MvvmCross.iOS.Views;
 using StartProject.Core.Domain.ViewModels;
+using MvvmCross.Binding.BindingContext;
 
 namespace StartProject.iOS
 {
@@ -10,6 +10,27 @@ namespace StartProject.iOS
     {
         public LoginViewController (IntPtr handle) : base (handle)
         {
+            View.BackgroundColor = UIColor.Cyan;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<LoginViewController, LoginViewModel>();
+
+            set.Bind(UsernameTextField)
+               .For("Placeholder")
+               .To(vm => vm.UsernamePlaceholder);
+          
+            set.Bind(LoginButton)
+               .For("Title")
+               .To(vm => vm.LoginButtonText);
+
+            set.Bind(LoginButton)
+               .To(vm => vm.LoginCommand);
+
+            set.Apply();
         }
     }
 }
